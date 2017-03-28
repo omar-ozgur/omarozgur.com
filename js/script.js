f4089f5ba819780a6215496bef76f506c24fa33f
Omar Ozgur <oozgur217@gmail.com>
2017-03-28 16:30:34 -0700

    Added projects and card layouts

--- js/script.js
+++ js/script.js
@@ -10,3 +10,19 @@ function ScrollToSection(){
 $(document).ready(function(){ 
 	ScrollToSection();
 });
+
+$(window).load(function() {
+  $('.post-module').hover(function() {
+    $(this).find('.post-content').stop(true, false).animate({top: '0px'}, 'fast', function() {
+      $(this).css("height", "300px");
+      $(this).find('.description').stop(true, false).fadeIn(100);
+    });
+  },
+  function() {
+    var content = $(this).find('.post-content');
+    $(this).find('.description').stop(true, false).fadeOut(100, function() {
+      content.css("height", "120px");
+      content.stop(true, false).animate({top: '180px'}, 'fast');
+    });
+  })
+});
